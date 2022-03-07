f0a32a21fddfa30859add350f34481f6b5379ca6
M16A2SA <62920122+M16A2SA@users.noreply.github.com>
2022-03-07 17:45:04 +0800

    Add files via upload

---
+++ bisectionMethod.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "methods.h"
+
+using namespace std;
+
+
+
+
+int main() {
+	cout << "function is y=log(x)" << endl;
+	cout << "it's zero point is: " << bisection(0, 2, 0.01);
+	return 0;
+}
---
+++ methods.h
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+typedef long long ll;
+const long double e = 2.7182818284590452;
+
+ll qpow(int a, int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else if(n % 2 == 1) {
+		return qpow(a, n - 1) * a;
+	}
+	else {
+		ll temp = qpow(a, n / 2);
+		return temp * temp;
+	}
+}
+
+
+long double function(long double x) {
+	long double y = log(x);
+	return y;
+}
+
+//a for left, b for right, c for middle, delta for allowed delta
+long double bisection(long double a, long double b, long double delta) {
+	long double c;
+	do {
+		c = (a + b) / 2;
+		if (::function(a) * ::function(c) < 0) {
+			b = c;
+			if (abs(a - b) < delta) {
+				return a;
+			}
+			else continue;
+		}
+
+		else {
+			if (::function(c) == 0) {
+				a = c;
+				return a;
+			}
+			else {
+				a = c;
+				if (abs(a - b) < delta) {
+					return a;
+				}
+				else continue;
+			}
+		}
+	} while (1);
+}
